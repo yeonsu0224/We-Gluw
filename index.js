d1ec2bbe7b577199bbb35e2763adaa8ba698e92d
연수 정 <yunsu0224@kaywon.ac.kr>
2025-06-10 16:31:04 +0900

    푸터 위치 수정

--- index.js
+++ index.js
@@ -12,8 +12,34 @@ let designSystem_Symbol_box = document.querySelector(".Symbol")
 let symbol_svg = document.querySelector(".symbol_svg")
 let symbol_grid = document.querySelector(".symbol_grid")
 
+//keyfunction 카드 스크롤 애니메이션을 위한 불러오기
+const functionTextBoxs = document.querySelectorAll(".functionTextBox")
+const functionCards = document.querySelectorAll(".function_list")
 
 
+//keyfunction 카드 스크롤 애니메이션
+
+const observerOptions = {
+  root: null, // 뷰포트 기준
+  rootMargin: "0px",
+  threshold: 0.5, // 50% 보이면 감지 (중앙쯤 오면)
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const index = entry.target.dataset.index;
+
+    if (entry.isIntersecting) {
+      // 먼저 모든 텍스트 카드 비활성화
+      functionTextBoxs.forEach((el) => el.classList.remove("on"));
+      // 해당 index만 활성화
+      functionTextBoxs[index].classList.add("on");
+    }
+  });
+}, observerOptions);
+
+// 각각의 카드 관찰 시작
+functionCards.forEach((card) => observer.observe(card));
 
 
 
@@ -25,16 +51,16 @@ coliving_board.addEventListener("click", ()=>{
 
 })
 
-designSystem_Symbol_box.addEventListener('mouseenter', () =>{
+setInterval(()=>{
   symbol_svg.classList.add("on")
   symbol_grid.classList.add("on")
+ 
+}, 3000)
 
-})
-
-designSystem_Symbol_box.addEventListener('mouseleave', () =>{
+ setInterval(() => {
   symbol_svg.classList.remove("on")
   symbol_grid.classList.remove("on")
-})
+}, 6000)
 
 
 
