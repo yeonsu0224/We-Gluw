e8605ccfdc1599261ba37793b2b75bb0ff020eff
연수 정 <yunsu0224@kaywon.ac.kr>
2025-06-18 22:32:26 +0900

    영상 배속 x2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const introVideo = document.getElementById("video");
 
-introVideo.playbackRate = 1.5;
+introVideo.playbackRate = 2;
 
 const videoSection = document.getElementById("introVideo_wrap");
 
